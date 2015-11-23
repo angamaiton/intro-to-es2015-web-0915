b837c20d1ac61e6676b05dedcc97505257e2746e
angamaiton <ofurmadur@icloud.com>
2015-11-23 11:09:02 -0500

    Adds Programmer class

---
+++ app/lib/programmer.js
@@ -0,0 +1,15 @@
+class Programmer {
+  constructor(name, language = "ruby") {
+    this.name = name;
+    this.language = language;
+  }
+  evangelize() {
+    return this.name + ": " + this.language.toUpperCase() + " IS THE BEST LANGUAGE EVER";
+  }
+}
+
+export var bestLanguages = ["Ruby", "Elixir"];
+export var addJavaScript = function(array) {
+  return array + "JavaScript";
+};
+export default Programmer;
